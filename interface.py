ba3200396be441c0c991cb6133d234890b44dc32
Ikey Doherty <ikey@solusos.com>
2013-09-13 20:31:40 +0100

    Component enumeration and selection

--- interface.py
+++ interface.py
@@ -24,7 +24,9 @@
 import gi.repository
 from gi.repository import Gtk
 import pisi.db.componentdb as componentdb
-
+import pisi.db.installdb as installdb
+import pisi.db.packagedb as packagedb
+import pisi.api
 
 class SSCWindow(Gtk.Window):
     
@@ -58,7 +60,13 @@ class SSCWindow(Gtk.Window):
         self.add(layout)
 
         self.componentdb = componentdb.ComponentDB()
+        self.installdb = installdb.InstallDB()
+        self.packagedb = packagedb.PackageDB()
+        
         self.components_view = Gtk.TreeView()
+        self.components_view.set_headers_visible(False)
+        selection = self.components_view.get_selection()
+        selection.connect("changed", self.open_component)
         ren = Gtk.CellRendererText()
         column = Gtk.TreeViewColumn("Component", ren)
         column.add_attribute(ren, "markup", 0)
@@ -68,10 +76,56 @@ class SSCWindow(Gtk.Window):
         scroller.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
         scroller.add(self.components_view)
 
+        self.treeview_packages = Gtk.TreeView()
+        
+        ren = Gtk.CellRendererText()
+        column = Gtk.TreeViewColumn("Package", ren)
+        column.add_attribute(ren, "markup", 0)
+        self.treeview_packages.append_column(column)
+        
+        ren = Gtk.CellRendererText()
+        column = Gtk.TreeViewColumn("Installed Version", ren)
+        column.add_attribute(ren, "markup", 1)
+        self.treeview_packages.append_column(column)
+
+        ren = Gtk.CellRendererText()
+        column = Gtk.TreeViewColumn("Available Version", ren)
+        column.add_attribute(ren, "markup", 2)
+        self.treeview_packages.append_column(column)
+
+        scroller2 = Gtk.ScrolledWindow(None, None)
+        scroller2.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        scroller2.add(self.treeview_packages)
+        
         center = Gtk.HBox()
         center.pack_start(scroller, False, False, 0)
+        center.pack_start(scroller2, True, True, 0)
         layout.pack_start(center, True, True, 0)
         self.build_components()
+
+    def open_component(self, selection):
+        model, treeiter = selection.get_selected()
+        if treeiter is None:
+            return
+        row = model[treeiter]
+        component = row[2]
+
+        self.build_packages(component)
+
+    def build_packages(self, component=None):
+        pkgs = component.packages
+        model = Gtk.ListStore(str,str,str)
+        for pkg in component.packages:
+            meta,pkg_ = pisi.api.info(pkg)
+            package = meta.package
+            n_vers = package.version
+            if self.installdb.has_package(pkg):
+                i_pkg = self.installdb.get_package(pkg)
+                model.append(["<big>%s</big>\n<small>%s</small>" % (package.name, package.summary), i_pkg.version, n_vers])
+            else:
+                model.append(["<big>%s</big>\n<small>%s</small>" % (package.name, package.summary), None, n_vers])
+        model.set_sort_column_id(0, Gtk.SortType.ASCENDING)
+        self.treeview_packages.set_model(model)
 
     def build_components(self):
         store = Gtk.ListStore(str,str,object)
