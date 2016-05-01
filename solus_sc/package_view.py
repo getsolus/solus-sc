c030dfa32a6c5e2223f20c68c9272391cd195d51
Ikey Doherty <ikey@solus-project.com>
2016-05-01 18:58:36 +0100

    Address column overlap with scrollbar
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- solus_sc/package_view.py
+++ solus_sc/package_view.py
@@ -27,6 +27,7 @@ class ScPackageView(Gtk.VBox):
         self.installdb = InstallDB()
 
         self.scroll = Gtk.ScrolledWindow(None, None)
+        self.scroll.set_overlay_scrolling(False)
         self.scroll.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
         self.scroll.set_property("kinetic-scrolling", True)
         self.pack_start(self.scroll, True, True, 0)
