e1dc6cfcb30fec074237ab873b07ec5c2ae7c88c
Ikey Doherty <ikey@solus-project.com>
2016-05-02 19:12:49 +0100

    Show some real updates from the repo now
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- solus_sc/updates_view.py
+++ solus_sc/updates_view.py
@@ -12,6 +12,11 @@
 #
 
 from gi.repository import Gtk, GLib, GdkPixbuf
+from pisi.db.packagedb import PackageDB
+from pisi.db.installdb import InstallDB
+
+import pisi.api
+
 
 PACKAGE_ICON_SECURITY = "security-high-symbolic"
 # software-update-urgent-symbolic ?
@@ -21,6 +26,7 @@ PACKAGE_ICON_NORMAL = "software-update-available-symbolic"
 class ScUpdatesView(Gtk.VBox):
 
     installdb = None
+    packagedb = None
     tview = None
 
     def __init__(self):
@@ -74,14 +80,46 @@ class ScUpdatesView(Gtk.VBox):
         model[path][4] = not model[path][4]
 
     def init_view(self):
+        # Need a shared context for these guys
+        self.installdb = InstallDB()
+        self.packagedb = PackageDB()
+
         model = Gtk.ListStore(str, str, str, str, bool, bool)
 
+        # Expand with a plan operation to be up front about new deps
+        upgrades = pisi.api.list_upgradable()
+        model.set_sort_column_id(0, Gtk.SortType.ASCENDING)
+
+        for item in upgrades:
+            new_pkg = self.packagedb.get_package(item)
+            new_version = "%s-%s" % (str(new_pkg.version),
+                                     str(new_pkg.release))
+            pkg_name = str(new_pkg.name)
+            old_pkg = None
+            old_version = "Not installed"
+            systemBase = False
+
+            icon = PACKAGE_ICON_NORMAL
+            if new_pkg.partOf == "system.base":
+                systemBase = True
+
+            if self.installdb.has_package(item):
+                old_pkg = self.installdb.get_package(item)
+                old_version = "%s-%s" % (str(old_pkg.version),
+                                         str(old_pkg.release))
+
+            model.append([pkg_name, old_version, new_version,
+                          icon, systemBase, not systemBase])
+            while (Gtk.events_pending()):
+                Gtk.main_iteration()
+
+        """
         model.append(["security-update", "old", "new",
                       PACKAGE_ICON_SECURITY, False, True])
         model.append(["non-security-update", "old", "new",
                       PACKAGE_ICON_NORMAL, False, True])
         model.append(["system.base update", "old", "new",
-                      PACKAGE_ICON_NORMAL, True, False])
+                      PACKAGE_ICON_NORMAL, True, False])"""
 
         self.tview.set_model(model)
         return False
