0556e77882e57b1697bb1de59150e31f63bc079e
Ikey Doherty <ikey@solus-project.com>
2016-06-15 22:26:15 +0100

    Begin making unfugly
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- solus_sc/components.py
+++ solus_sc/components.py
@@ -13,6 +13,65 @@
 
 from gi.repository import Gtk
 
+ICON_MAPS = {
+    "database": "office-database",
+    "desktop": "user-desktop",
+    "desktop.budgie": "start-here-solus",
+    "desktop.core": "system-run",
+    "desktop.gnome": "start-here-symbolic",
+    "desktop.gnome.core": "system-devices-information",
+    "desktop.gnome.doc": "folder-documents",
+    "desktop.gtk": "gtk-dialog-info",
+    "desktop.library": "emblem-shared-symbolic",
+    "desktop.multimedia": "multimedia-volume-control",
+    "desktop.qt": "qtconfig-qt4",
+    "editor": "x-office-calendar",
+    "office": "x-office-spreadsheet",
+}
+
+
+class ScComponentButton(Gtk.Button):
+    """ Manage the monotony of a button """
+
+    component = None
+
+    def __init__(self, db, component):
+        Gtk.Button.__init__(self)
+
+        self.component = component
+
+        c_desc = str(component.localName)
+        if component.name in ICON_MAPS:
+            icon = ICON_MAPS[component.name]
+        else:
+            icon = "package-x-generic"
+        image = Gtk.Image.new_from_icon_name(icon, Gtk.IconSize.SMALL_TOOLBAR)
+        image.set_halign(Gtk.Align.START)
+        # image.set_pixel_size(64)
+
+        label_box = Gtk.VBox(0)
+
+        box = Gtk.HBox(0)
+        box.pack_start(image, False, False, 0)
+        image.set_property("margin-right", 10)
+        label = Gtk.Label(c_desc)
+        label.get_style_context().add_class("title")
+        label.set_halign(Gtk.Align.START)
+        label.set_valign(Gtk.Align.START)
+        label_box.pack_start(label, True, True, 0)
+        box.pack_start(label_box, True, True, 0)
+        # self.set_relief(Gtk.ReliefStyle.NONE)
+        self.add(box)
+
+        # count the components
+        count = len(db.get_packages(component.name))
+        info_label = Gtk.Label(str(count))
+        info_label.set_halign(Gtk.Align.START)
+        info_label.get_style_context().add_class("info-label")
+        info_label.get_style_context().add_class("dim-label")
+        # label_box.pack_start(info_label, False, False, 0)
+
+        self.get_style_context().add_class("group-button")
 
 class ScComponentsView(Gtk.EventBox):
     """ Main group view, i.e. "System", "Development", etc. """
@@ -33,6 +92,9 @@ class ScComponentsView(Gtk.EventBox):
         self.flowbox = Gtk.FlowBox()
         self.flowbox.set_property("margin-start", 40)
         self.flowbox.set_property("margin-end", 40)
+        self.flowbox.set_property("margin-top", 20)
+        self.flowbox.set_column_spacing(10)
+        self.flowbox.set_row_spacing(10)
         self.flowbox.set_orientation(Gtk.Orientation.HORIZONTAL)
         self.flowbox.set_selection_mode(Gtk.SelectionMode.NONE)
         self.flowbox.set_valign(Gtk.Align.START)
@@ -45,6 +107,7 @@ class ScComponentsView(Gtk.EventBox):
         compdb = self.owner.basket.componentdb
         for comp in components:
             component = compdb.get_component(comp)
-            btn = Gtk.Button(component.localName)
+            btn = ScComponentButton(compdb, component)
             self.flowbox.add(btn)
             btn.show_all()
+            print(component.name)
