25584be4b9f21f92df5ef5eb13d13195655a3083
Ikey Doherty <ikey@solus-project.com>
2016-06-15 02:25:18 +0100

    Pop a menu into it
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- solus_sc/tray.py
+++ solus_sc/tray.py
@@ -17,8 +17,26 @@ from gi.repository import Gtk
 
 class ScTray(Gtk.StatusIcon):
 
+    # Actual popup menu
+    menu = None
+
     def __init__(self):
         Gtk.StatusIcon.__init__(self)
         self.set_from_icon_name("start-here-solus")
         self.set_visible(True)
         self.set_tooltip_text("Software Center")
+
+        self.menu = Gtk.Menu()
+        quit_item = Gtk.MenuItem("Quit")
+        self.menu.append(quit_item)
+        quit_item.show()
+
+        self.connect("popup-menu", self.on_popup)
+        self.connect("activate", self.on_activate)
+
+    def on_activate(self, tr):
+        print("Activating..")
+
+    def on_popup(self, tray, button, whence):
+        self.menu.popup(None, None, None, None, button, whence)
+        return True
