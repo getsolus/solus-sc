ebaaed5bb2bec4b7528488dd65cc2e4ec77a587f
Ikey Doherty <ikey@solus-project.com>
2016-06-19 11:01:16 +0100

    groups: Use clearer notation in the info string
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- solus_sc/groups.py
+++ solus_sc/groups.py
@@ -68,7 +68,7 @@ class ScGroupButton(Gtk.Button):
 
         # count the components
         kids = db.get_group_components(group.name)
-        info_label = Gtk.Label("%s sections" % len(kids))
+        info_label = Gtk.Label("%s categories" % len(kids))
         info_label.set_halign(Gtk.Align.START)
         info_label.get_style_context().add_class("info-label")
         info_label.get_style_context().add_class("dim-label")
