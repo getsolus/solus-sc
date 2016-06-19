a06ef14615aec5c820485190550d64ba57b0fa19
Ikey Doherty <ikey@solus-project.com>
2016-06-19 16:44:02 +0100

    Start to flesh out the Third Party UI
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- solus_sc/thirdparty.py
+++ solus_sc/thirdparty.py
@@ -13,9 +13,51 @@
 
 from gi.repository import Gtk
 
+APPS = {
+    'spotify':
+        ('Spotify', 'spotify',
+         'Spotify Music, Video and Podcast Streaming Client.',
+         'multimedia/music/spotify/pspec.xml'),
+    'google-chrome-stable':
+        ('Google Chrome', 'google-chrome',
+         'The web browser from Google',
+         'network/web/browser/google-chrome-stable/pspec.xml'),
+}
+
 
 class ThirdPartyView(Gtk.VBox):
     """ Work around insane distribution policy to help the user. """
 
+    listbox = None
+
     def __init__(self, owner):
         Gtk.VBox.__init__(self)
+
+        self.listbox = Gtk.ListBox()
+        self.listbox.set_selection_mode(Gtk.SelectionMode.NONE)
+        scroll = Gtk.ScrolledWindow(None, None)
+        self.pack_start(scroll, True, True, 0)
+        scroll.add(self.listbox)
+
+        self.build_ui()
+
+    def build_ui(self):
+        for child in self.listbox.get_children():
+            child.destroy()
+
+        for key in APPS:
+            row = APPS[key]
+
+            hbox = Gtk.HBox(0)
+            img = Gtk.Image.new_from_icon_name(row[1], Gtk.IconSize.DIALOG)
+            hbox.pack_start(img, False, False, 0)
+            img.set_property("margin", 6)
+
+            nom_box = Gtk.Label("<big>{}</big>\n{}".format(row[0], row[2]))
+            nom_box.set_use_markup(True)
+            nom_box.set_line_wrap(True)
+            nom_box.set_property("xalign", 0.0)
+            nom_box.set_property("margin", 6)
+            hbox.pack_start(nom_box, True, True, 0)
+
+            self.listbox.add(hbox)
