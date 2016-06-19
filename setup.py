532c50c08b4e95325e3916dfab026bc7f3de607d
Ikey Doherty <ikey@solus-project.com>
2016-06-19 18:37:49 +0100

    Begin the new backend implementation
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- setup.py
+++ setup.py
@@ -8,12 +8,13 @@ setup(
     description     = ("Solus Software Center"),
     license         = "GPL-2.0",
     url             = "https://github.com/solus-project/os-installer",
-    packages        = ['solus_sc'],
+    packages        = ['solus_sc', 'eopkg_assist'],
     scripts         = ['solus-sc'],
     classifiers     = [ "License :: OSI Approved :: GPL-2.0 License"],
     package_data    = {'solus_sc': ['update_dialog.ui', 'styling.css']},
     data_files      = [("/usr/share/applications", ["data/solus-sc.desktop"]),
                        ("/usr/share/dbus-1/system-services", ["data/dbus-1/system-services/com.solus_project.eopkgassist.service"]),
                        ("/etc/dbus-1/system.d", ["data/system.d/com.solus_project.eopkgassist.conf"]),
-                       ("/usr/share/polkit-1/actions", ["data/polkit-1/actions/com.solus_project.eopkgassist.policy"])]
+                       ("/usr/share/polkit-1/actions", ["data/polkit-1/actions/com.solus_project.eopkgassist.policy"]),
+                       ("/usr/libexec", ["eopkg-assist-wrapper"])]
 )
