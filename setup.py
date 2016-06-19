106568ae62fb9e6aca04cf875cbc7ab3b8e958e6
Ikey Doherty <ikey@solus-project.com>
2016-06-19 15:53:38 +0100

    Add the setup.py
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name            = "solus-sc",
+    version         = "6.0",
+    author          = "Ikey Doherty",
+    author_email    = "ikey@solus-project.com",
+    description     = ("Solus Software Center"),
+    license         = "GPL-2.0",
+    url             = "https://github.com/solus-project/os-installer",
+    packages        = ['solus_sc'],
+    scripts         = ['solus-sc'],
+    classifiers     = [ "License :: OSI Approved :: GPL-2.0 License"],
+    package_data    = {'solus_sc': ['update_dialog.ui', 'styling.css']},
+)
