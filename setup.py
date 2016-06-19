6adeb0f3eb5267b927ba023d345a8dd5009285d0
Ikey Doherty <ikey@solus-project.com>
2016-06-19 18:27:20 +0100

    Install the d-bus/polkit support files in setup.py
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- setup.py
+++ setup.py
@@ -12,5 +12,8 @@ setup(
     scripts         = ['solus-sc'],
     classifiers     = [ "License :: OSI Approved :: GPL-2.0 License"],
     package_data    = {'solus_sc': ['update_dialog.ui', 'styling.css']},
-    data_files      = [("/usr/share/applications", ["data/solus-sc.desktop"])]
+    data_files      = [("/usr/share/applications", ["data/solus-sc.desktop"]),
+                       ("/usr/share/dbus-1/system-services", ["data/dbus-1/system-services/com.solus_project.eopkgassist.service"]),
+                       ("/etc/dbus-1/system.d", ["data/system.d/com.solus_project.eopkgassist.conf"]),
+                       ("/usr/share/polkit-1/actions", ["data/polkit-1/actions/com.solus_project.eopkgassist.policy"])]
 )
