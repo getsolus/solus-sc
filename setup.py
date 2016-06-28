5f27bb0b2d1dceee7e0b687017243c0e4d0a7288
Ikey Doherty <ikey@solus-project.com>
2016-06-29 00:05:57 +0100

    Bump 12.1
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name            = "solus-sc",
-    version         = "12.0",
+    version         = "12.1",
     author          = "Ikey Doherty",
     author_email    = "ikey@solus-project.com",
     description     = ("Solus Software Center"),
