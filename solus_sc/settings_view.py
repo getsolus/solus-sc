09099134accf1339ac50427f7178a878682f31eb
Ikey Doherty <ikey@solus-project.com>
2016-10-19 01:54:39 +0100

    Hook up the button to check for updates
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- solus_sc/settings_view.py
+++ solus_sc/settings_view.py
@@ -11,12 +11,15 @@
 #  (at your option) any later version.
 #
 
-from gi.repository import Gtk
+from gi.repository import Gtk, Gio
 from . import join_resource_path
 
 
 class ScSettingsView(Gtk.EventBox):
     """ Settings for updates, etc """
+
+    settings = None
+    check_button = None
 
     def can_back(self):
         """ Whether we can go back """
@@ -30,6 +33,21 @@ class ScSettingsView(Gtk.EventBox):
 
         builder.add_from_file(rp)
 
+        # Main view itself
         main_grid = builder.get_object("main_grid")
-
         self.add(main_grid)
+
+        # Check for updates
+        self.check_button = builder.get_object("switch_check_updates")
+
+        # Hook up settings
+        self.settings = Gio.Settings.new("com.solus-project.software-center")
+        self.settings.bind("check-updates",
+                           self.check_button, "active",
+                           Gio.SettingsBindFlags.DEFAULT)
+
+        self.settings.connect("changed", self.on_settings_changed)
+
+    def on_settings_changed(self, key, udata=None):
+        """ Handle settings changes """
+        pass
