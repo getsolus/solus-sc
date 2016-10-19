ceef64ed6ebdde360424211571baedb062460605
Ikey Doherty <ikey@solus-project.com>
2016-10-19 01:47:25 +0100

    Fix resource cruft
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- solus_sc/__init__.py
+++ solus_sc/__init__.py
@@ -12,6 +12,17 @@
 #
 
 import gi.repository
+import os
+
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gio', '2.0')
 gi.require_version('AppStreamGlib', '1.0')
+
+
+def get_resource_path():
+    bsPath = os.path.dirname(__file__)
+    return os.path.join(bsPath, "data")
+
+
+def join_resource_path(path):
+    return os.path.join(get_resource_path(), path)
