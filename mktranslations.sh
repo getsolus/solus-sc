40753805f6e8bd81fee9b92dd40fc2c72db6c871
Ikey Doherty <ikey@solus-project.com>
2016-10-20 07:37:52 +0100

    Add simple script for managing translations
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ mktranslations.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+set -e
+
+if [[ ! -d po ]]; then
+    mkdir po
+fi
+
+grep "_(\"" */*.py|cut -d : -f 1 | uniq > list1
+grep "_(\'" */*.py|cut -d : -f 1 | uniq > list2
+cat list1 list2 > list3
+cat list3 | uniq | sort > po/POTFILES.in
+rm list1 list2 list3
+
+python2.7 setup.py build_i18n -m 
