3ca2d6eb3cc417ef5167706ae1315eee0750eddf
Ikey Doherty <ikey@solus-project.com>
2016-10-20 07:20:53 +0100

    Make flake8 not freak out
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pep8 solus_sc/*.py solus_update/*.py solus-sc solus-update-checker eopkg_assist/*.py || exit 1
-flake8 solus_sc/*.py solus_update/*.py solus-sc solus-update-checker eopkg_assist/*.py || exit 1
+flake8 --builtins="_" solus_sc/*.py solus_update/*.py solus-sc solus-update-checker eopkg_assist/*.py || exit 1
 
