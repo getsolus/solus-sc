1c01779889918da03a32e6eaa293aabf2d495150
Ikey Doherty <ikey@solus-project.com>
2017-04-20 03:43:48 +0100

    Update mktranslations to account for our XML repo fils (issue #87)
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- mktranslations.sh
+++ mktranslations.sh
@@ -10,6 +10,10 @@ fi
 grep "_(\"" */*.py|cut -d : -f 1 | uniq > list1
 grep "_(\'" */*.py|cut -d : -f 1 | uniq > list2
 cat list1 list2 > list3
+# Merge xml files into final translations
+for i in repo_data/*.xml.in; do
+    echo "$i" >> list3
+done
 cat list3 | uniq | sort > po/POTFILES.in
 rm list1 list2 list3
 
