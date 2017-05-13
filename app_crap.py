124246e1dbeb818ca15930fe0b4fa0751aaee386
Ikey Doherty <ikey@solus-project.com>
2017-05-13 17:52:10 +0100

    app_crap: Add function for constructing unique screenshot paths

--- app_crap.py
+++ app_crap.py
@@ -3,6 +3,8 @@ import gi.repository
 gi.require_version('AppStreamGlib', '1.0')
 from gi.repository import AppStreamGlib as AS
 import sys
+import hashlib
+import os
 
 store = AS.Store()
 
@@ -48,9 +50,25 @@ for image in images:
     if large and normal and thumbnail:
         break
 
+def get_cache_dir():
+    return os.path.join(os.path.expanduser("~"), ".cache", "solus-sc", "screens")
+
+def get_image_filename(img):
+    url = img.get_url()
+    f, ext = os.path.splitext(url)
+    h = hashlib.sha256(f).hexdigest()
+    return "{}.{}".format(h, ext[1:])
+
+def get_image_filename_full(img):
+    return os.path.join(get_cache_dir(), get_image_filename(img))
+
 if thumbnail:
     print("Thumbnail: %s" % thumbnail.get_url())
+    print(get_image_filename_full(thumbnail))
 if normal:
     print("Normal: %s" % normal.get_url())
+    print(get_image_filename_full(normal))
 if large:
     print("Large: %s" % large.get_url())
+    print(get_image_filename_full(large))
+
