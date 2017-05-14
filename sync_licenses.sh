5e26e779e5222e255eeb62a421a808b079f30d99
Ikey Doherty <ikey@solus-project.com>
2017-05-14 17:27:54 +0100

    Add scripts for grabbing SPDX records
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ sync_licenses.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+fail_exit(){
+    echo "$1"
+    exit 1
+}
+
+git clone https://github.com/spdx/license-list.git --depth=1 || fail_exit "Failed to clone"
+
+if [[ -e "licenses.spdx" ]];
+    then rm -v licenses.spdx
+fi
+
+pushd license-list
+
+echo "licenses = [" > licenses.spdx
+for i in *.txt ; do
+    nom=`echo "$i" | sed 's@\.txt$@@'`
+    echo -e "    \"${nom}\"," >> ../licenses.spdx
+done
+popd
+rm -rf license-list
+echo "]" >> licenses.spdx
