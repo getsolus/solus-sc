336b69ab3c42a85f86e53eb2526b7f819101ce3d
Ikey Doherty <ikey@solus-project.com>
2017-09-25 16:40:34 +0100

    Begin working on a new abstract API design
    The new API will enable us to abstract our existing items available in
    the repositories into a simpler set of plugins that populate storage
    classes.
    
    Each ProviderPlugin (eopkg, snapd) will be asked at different times to
    populate a storage object. This storage object may at times be transient
    and useful only for the current transaction, such as listing installed
    packages or performing a search query.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ snapTests/storage.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+#  This file is part of solus-sc
+#
+#  Copyright © 2017 Ikey Doherty <ikey@solus-project.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+
+from enum import Enum
+
+class PopulationFilter(Enum):
+    """ A population filter is provided to the provider plugin to begin
+        population of a given type
+    """
+
+    INSTALLED = 0
+    SEARCH = 1
+    CATEGORY = 2
+
+class ProviderStorage:
+    """ ProviderStorage is an abstract type that should be populated by
+        existing plugins
+
+        Storage may be recycled at any time and is used simply to allow
+        dynamic "pushing" of items into the storage
+    """
+
+    def add_item(self, id, item):
+        raise RuntimeError("implement add_item")
+
+    def clear(self):
+        raise RuntimeError("implement clear")
+
+class ProviderPlugin:
+    """ A ProviderPlugin provides its own managemenet and access to the
+        underlying package management system to provide the options to the
+        user
+    """
+
+    def populate_storage(self, storage, popfilter, extra):
+        """ Populate storage using the given filter """
+        raise RuntimeError("implement populate_installed")
+
+    def cancel(self):
+        """ Cancel any ongoing populare_storage calls """
+        raise RuntimeError("implement cancel")
+
+
+class ProviderItem:
+    """ A ProviderItem is addded to the ProviderStorage by each ProviderPlugin
+        and enables access + caching of various backend package management
+        systems
+    """
+
+    def get_id(self):
+        """ Every item should return their unique ID so that they can
+            be tracked and differentiated between different backends
+        """
+        raise RuntimeError("implement get_id")
+
+    def get_title(self):
+        """ Each item should return an appropriate item for displaying
+            as the stylised title
+        """
+        raise RuntimeError("implement get_title")
+
+    def get_description(self):
+        """ Each item should support returning their complete description
+        """
+        raise RuntimeError("implement get_description")
+
+    def get_version(self):
+        """ Each item should return a usable version string. This is purely
+            for cosmectics
+        """
+        raise RuntimeError("implement get_version")
