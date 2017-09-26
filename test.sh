c42ec2baf7f7bfe20694bff20ca8b14092bfd328
Ikey Doherty <ikey@solus-project.com>
2017-09-26 17:04:00 +0100

    Make test.sh work again
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@ flake8 --builtins="_" solus_sc/*.py solus_update/*.py solus-sc solus-update-chec
 
 # Now for our staging tree
 
-pep8 snapTests/*-* snapTests/plugins/*.py || exit 1
-flake8 --builtins="_" snapTests/*-* snapTests/plugins/*.py || exit 1
+pep8 api-test next_sc/plugins/*.py || exit 1
+flake8 --builtins="_" api-test next_sc/plugins/*.py || exit 1
 
