283ef97b9c95c45a5c71b7ecc5a2e19aa848cece
Ikey Doherty <ikey@solus-project.com>
2017-09-26 17:02:47 +0100

    Port to next_sc namespace
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ next_sc/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+#  This file is part of solus-sc
+#
+#  Copyright © 2017 Ikey Doherty <ikey@solus-project.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
