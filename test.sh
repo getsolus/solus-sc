bb70a144f39680e0e6aeba3c3fc71eb6d1a04f2d
Ikey Doherty <ikey@solus-project.com>
2017-09-26 15:23:18 +0100

    Get the new test code under test.sh
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- test.sh
+++ test.sh
@@ -3,3 +3,8 @@
 pep8 solus_sc/*.py solus_update/*.py solus-sc solus-update-checker eopkg_assist/*.py || exit 1
 flake8 --builtins="_" solus_sc/*.py solus_update/*.py solus-sc solus-update-checker eopkg_assist/*.py || exit 1
 
+# Now for our staging tree
+
+pep8 snapTests/*-* snapTests/plugins/*.py || exit 1
+flake8 --builtins="_" snapTests/*-* snapTests/plugins/*.py || exit 1
+
