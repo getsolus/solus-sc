70fc6d771b7d10456128c4a36daaad0b94e7e0f1
Ikey Doherty <ikey@solus-project.com>
2017-09-28 21:06:57 +0100

    appsystem: Restore magical pixbuf methods
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- next_sc/appsystem.py
+++ next_sc/appsystem.py
@@ -27,12 +27,51 @@ class AppSystem:
 
     store = None
 
+    store = None
+    default_pixbuf = None
+    security_pixbuf = None
+    mandatory_pixbuf = None
+    other_pixbuf = None
+    addon_pixbuf = None
+    fetcher = None
+
     def __init__(self):
         self.store = As.Store()
         self.store.load(As.StoreLoadFlags.APP_INFO_SYSTEM)
 
+        itheme = Gtk.IconTheme.get_default()
+        try:
+            self.default_pixbuf = self.scaled_icon(itheme.load_icon(
+                "package-x-generic",
+                64,
+                Gtk.IconLookupFlags.GENERIC_FALLBACK))
+            self.security_pixbuf = self.scaled_icon(itheme.load_icon(
+                "network-vpn",
+                64,
+                Gtk.IconLookupFlags.GENERIC_FALLBACK))
+            self.mandatory_pixbuf = self.scaled_icon(itheme.load_icon(
+                "computer",
+                64,
+                Gtk.IconLookupFlags.GENERIC_FALLBACK))
+            self.other_pixbuf = self.scaled_icon(itheme.load_icon(
+                "folder-download",
+                64,
+                Gtk.IconLookupFlags.GENERIC_FALLBACK))
+            self.addon_pixbuf = self.scaled_icon(itheme.load_icon(
+                "application-x-addon",
+                64,
+                Gtk.IconLookupFlags.GENERIC_FALLBACK))
+        except Exception as e:
+            print(e)
+
     def sanitize(self, text):
         return text.replace("&quot;", "\"")
+
+    def scaled_icon(self, pbuf):
+        if pbuf.get_height() != 64:
+            pbuf = pbuf.scale_simple(
+                64, 64, GdkPixbuf.InterpType.BILINEAR)
+        return pbuf
 
     def get_summary(self, id, fallback):
         """ Return a usable summary for a package """
@@ -84,6 +123,70 @@ class AppSystem:
         if fallback:
             return str(fallback)
         return None
+
+    def get_pixbuf(self, package):
+        """ Get the AppStream GdkPixbuf for a package """
+        app = self.store.get_app_by_pkgname(package.name)
+        if not app:
+            return None
+        # TODO: Incorporate HIDPI!
+        icon = app.get_icon_for_size(64, 64)
+        if not icon:
+            return self.default_pixbuf_lookup(app)
+        kind = icon.get_kind()
+        if kind == As.IconKind.UNKNOWN or kind == As.IconKind.REMOTE:
+            return None
+        if kind == As.IconKind.STOCK:
+            # Load up a gthemedicon version
+            im = Gio.ThemedIcon.new(icon.get_name())
+            return im
+        if not icon.load(As.IconLoadFlags.SEARCH_SIZE):
+            return None
+        return icon.get_pixbuf()
+
+    def default_pixbuf_lookup(self, app):
+        """ Use our built in preloaded pixbufs """
+        if app is None:
+            return self.default_pixbuf
+        kind = app.get_kind()
+        if kind == As.AppKind.ADDON:
+            return self.addon_pixbuf
+        return self.default_pixbuf
+
+    def get_pixbuf_only(self, id):
+        """ Only get a pixbuf - no fallbacks  """
+        app = self.store.get_app_by_pkgname(id)
+        if not app:
+            return self.default_pixbuf_lookup(app)
+        # TODO: Incorporate HIDPI!
+        icon = app.get_icon_for_size(64, 64)
+        if not icon:
+            return self.default_pixbuf_lookup(app)
+        kind = icon.get_kind()
+        if kind == As.IconKind.UNKNOWN or kind == As.IconKind.REMOTE:
+            return None
+        if kind == As.IconKind.STOCK:
+            # Load up a gthemedicon version
+            try:
+                itheme = Gtk.IconTheme.get_default()
+                pbuf = itheme.load_icon(
+                    icon.get_name(),
+                    64,
+                    Gtk.IconLookupFlags.GENERIC_FALLBACK)
+                if pbuf.get_height() != 64:
+                    pbuf = pbuf.scale_simple(
+                        64, 64, GdkPixbuf.InterpType.BILINEAR)
+                return pbuf
+            except Exception as e:
+                print(e)
+            return self.default_pixbuf
+        if not icon.load(As.IconLoadFlags.SEARCH_SIZE):
+            return self.default_pixbuf
+        pbuf = icon.get_pixbuf()
+        if pbuf.get_height() != 64:
+            pbuf = pbuf.scale_simple(
+                64, 64, GdkPixbuf.InterpType.BILINEAR)
+        return pbuf
 
     def get_donation_site(self, id):
         """ Get a donation link for the given package """
