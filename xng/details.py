4569558279c90fc95deee013f536c8f612f0b6eb
Ikey Doherty <ikey@solus-project.com>
2017-12-04 17:34:52 +0000

    Super basic details view
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- xng/details.py
+++ xng/details.py
@@ -22,19 +22,75 @@ class ScDetailsView(Gtk.Box):
         installation, etc.
     """
 
+    __gtype_name__ = "ScDetailsView"
+
     context = None
     item = None
 
-    __gtype_name__ = "ScDetailsView"
+    # Header widgets
+    header_name = None
+    header_image = None
+    header_summary = None
+
+    # TODO: Make less dumb
+    header_action = None
 
     def __init__(self, context):
         Gtk.Box.__init__(self, orientation=Gtk.Orientation.VERTICAL)
 
         self.context = context
+
+        self.build_header()
         self.show_all()
 
     def set_item(self, item):
         """ Update our UI for the current item """
         if item == self.item:
             return
+
+        apps = self.context.appsystem
+        id = item.get_id()
+
+        self.header_name.set_markup(apps.get_name(id, item.get_name()))
+        self.header_summary.set_markup(
+            apps.get_summary(id, item.get_summary()))
+        pbuf = apps.get_pixbuf_only(id)
+        self.header_image.set_from_pixbuf(pbuf)
         print("got updated")
+
+    def build_header(self):
+        """ Build our main header area """
+        box = Gtk.Box.new(Gtk.Orientation.HORIZONTAL, 0)
+        box.set_border_width(20)
+        ebox = Gtk.EventBox()
+        ebox.add(box)
+        ebox.get_style_context().add_class("details-header")
+        self.pack_start(ebox, False, False, 0)
+
+        self.header_name = Gtk.Label("")
+        self.header_name.get_style_context().add_class("huge-label")
+        self.header_image = Gtk.Image()
+        self.header_image.set_margin_end(24)
+        self.header_image.set_margin_start(12)
+
+        box.pack_start(self.header_image, False, False, 0)
+
+        details_box = Gtk.Box.new(Gtk.Orientation.VERTICAL, 0)
+        box.pack_start(details_box, True, True, 0)
+
+        # name
+        self.header_name.set_halign(Gtk.Align.START)
+        self.header_name.set_halign(Gtk.Align.START)
+        details_box.pack_start(self.header_name, True, True, 0)
+
+        # summary
+        self.header_summary = Gtk.Label("")
+        self.header_summary.set_margin_top(6)
+        self.header_summary.set_margin_bottom(6)
+        self.header_summary.set_halign(Gtk.Align.START)
+        details_box.pack_start(self.header_summary, False, False, 0)
+
+        # actions
+        self.header_action = Gtk.Button("Install")
+        self.header_action.set_valign(Gtk.Align.CENTER)
+        box.pack_end(self.header_action, False, False, 0)
