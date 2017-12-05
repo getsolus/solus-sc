a94fa0acf9ba8dfbb025795c3158aa766258a3df
Ikey Doherty <ikey@solus-project.com>
2017-12-05 10:51:14 +0000

    Start new odrs module
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 #pep8 solus_sc/*.py solus_update/*.py solus-sc solus-update-checker eopkg_assist/*.py || exit 1
 #flake8 --builtins="_" solus_sc/*.py solus_update/*.py solus-sc solus-update-checker eopkg_assist/*.py || exit 1
 
-pep8 xng/*.py xng/plugins/*.py new.py || exit 1
-flake8 --builtins="_" xng/*.py xng/plugins/*.py new.py || exit 1
+pep8 xng/*.py xng/plugins/*.py odrs/*.py new.py || exit 1
+flake8 --builtins="_" xng/*.py xng/plugins/*.py odrs/*.py new.py || exit 1
 
