885d2b27fccdd4d149aa2d9011ab86caf4927f4c
Ikey Doherty <ikey@solus-project.com>
2017-12-07 13:07:43 +0000

    xng: Import our OpQueue/Executor from the snap fest
    These guys are actually what we use to apply our operational stack and
    allow updates/removals to happen.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ xng/executor.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+#  This file is part of solus-sc
+#
+#  Copyright © 2017 Ikey Doherty <ikey@solus-project.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+
+
+from .op_queue import OperationQueue, Operation, OperationType
+from threading import Lock, Thread
+
+
+class Executor:
+    """ Executor is responsible for handling the main "loop" around the
+        installation/removal of packages
+    """
+
+    queue = None
+    thread_lock = None
+    thread_running = False
+
+    def __init__(self):
+        self.queue = OperationQueue()
+        self.thread_lock = Lock()
+        self.thread_running = False
+
+        # TODO: Spawn a thread
+
+    def install_package(self, ids):
+        """ Install or queue installation """
+        self.queue.push_operation(Operation.Install(ids))
+        self.maybe_respawn()
+
+    def remove_package(self, ids):
+        """ Remove or queue removal """
+        self.queue.push_operation(Operation.Remove(ids))
+        self.maybe_respawn()
+
+    def upgrade_package(self, ids):
+        """ Upgrade or queue upgrade """
+        self.queue.push_operation(Operation.Upgrade(ids))
+        self.maybe_respawn()
+
+    def maybe_respawn(self):
+        """ Start up the worker thread again if our thread ended """
+        self.thread_lock.acquire()
+        try:
+            if not self.thread_running:
+                self.thread_running = True
+                print("debug: spawning new work thread")
+                t = Thread(target=self.process_queue)
+                t.start()
+            else:
+                print("debug: thread is already running")
+        finally:
+            self.thread_lock.release()
+
+    def process_queue(self):
+        """ Process the queue until it empties """
+        while not self.queue.opstack.empty():
+            item = self.queue.opstack.get()
+            plugin = item.data.get_plugin()
+            if item.opType == OperationType.INSTALL:
+                plugin.install_item(item.data)
+            elif item.opType == OperationType.REMOVE:
+                plugin.install_item(item.data)
+            elif item.opType == OperationType.UPGRADE:
+                plugin.remove_item(item.data)
+            print("Got item: {}".format(item.data))
+        # Queue ran out
+        print("queue emptied")
+        self.thread_lock.acquire()
+        try:
+            self.thread_running = False
+        finally:
+            self.thread_lock.release()
---
+++ xng/op_queue.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+#  This file is part of solus-sc
+#
+#  Copyright © 2017 Ikey Doherty <ikey@solus-project.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+
+import Queue
+
+
+class OperationType:
+
+    INSTALL = 2  # Installs should be evaluated after removals
+    REMOVE = 1   # Remove is low-ish priority but must be before installs
+    UPGRADE = 0  # Upgrades should be before everything!
+
+
+class Operation:
+    """ Operation wraps up various operations so that they can be applied
+        immediately whilst other operations will continue to be stacked up
+        until such time as they can be applied.
+    """
+
+    opType = 0
+    data = None
+
+    def __cmp__(self, other):
+        """ Ensure we can make other items higher priority ... """
+        return cmp(self.opType, other.opType)
+
+    def __init__(self, data, opType):
+        self.data = data
+        self.opType = opType
+
+    @staticmethod
+    def Install(ids):
+        return Operation(ids, OperationType.INSTALL)
+
+    @staticmethod
+    def Remove(ids):
+        return Operation(ids, OperationType.REMOVE)
+
+    @staticmethod
+    def Upgrade(ids):
+        return Operation(ids, OperationType.UPGRADE)
+
+
+class OperationQueue:
+    """ OperationQueue is used to apply any pending packaging operations
+        within the Software Center one after another
+    """
+
+    # We enqueue pending operations here
+    opstack = None
+
+    def __init__(self):
+        self.opstack = Queue.PriorityQueue(0)
+
+    def push_operation(self, op):
+        """ Set up an operation to be applied """
+        self.opstack.put(op)
