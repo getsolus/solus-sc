0dfca67eb8147d221ee30e6fd4d5af35ad3742fb
Ikey Doherty <ikey@solus-project.com>
2017-12-07 12:39:13 +0000

    plugins: Teach eopkg to mark updates + installed items
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- xng/plugins/eopkg.py
+++ xng/plugins/eopkg.py
@@ -314,6 +314,11 @@ class EopkgItem(ProviderItem):
 
         if self.installed is not None:
             self.displayCandidate = self.installed
+            self.add_status(ItemStatus.INSTALLED)
+            relNew = self.available.history[0].release
+            relOld = self.installed.history[0].release
+            if relNew > relOld:
+                self.add_status(ItemStatus.UPDATE_NEEDED)
         else:
             self.displayCandidate = self.available
 
