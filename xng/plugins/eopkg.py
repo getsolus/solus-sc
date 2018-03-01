23c502d10dc2e9a4c37a8bc91551b9250bc605c2
Ikey Doherty <ikey@solus-project.com>
2018-03-01 16:56:39 +0000

    plugins: Automatically assign eopkg components an icon if we can
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- xng/plugins/eopkg.py
+++ xng/plugins/eopkg.py
@@ -85,6 +85,58 @@ essential_packages = [
 ]
 
 
+# Utter laziness. :P
+ICON_MAPS = {
+    "desktop": "user-desktop",
+    "desktop.budgie": "start-here-solus",
+    "desktop.core": "system-run",
+    "desktop.font": "fonts",
+    "desktop.gnome": "desktop-environment-gnome",
+    "desktop.gnome.core": "system-devices-information",
+    "desktop.gnome.doc": "folder-documents",
+    "desktop.gtk": "gtk-dialog-info",
+    "desktop.kde": "desktop-environment-kde",
+    "desktop.library": "emblem-shared-symbolic",
+    "desktop.mate": "mate",
+    "desktop.multimedia": "multimedia-volume-control",
+    "desktop.qt": "qtconfig-qt4",
+    "desktop.theme": "preferences-desktop-wallpaper",
+    "editor": "x-office-document",
+    "games": "applications-games",
+    "games.action": "dota2",
+    "games.arcade": "gnome-nibbles",
+    "games.card": "gnome-aisleriot",
+    "games.emulator": "ds-emulator",
+    "games.puzzle": "gnome-tetravex",
+    "games.rpg": "wesnoth",
+    "games.strategy": "games-endturn",
+    "multimedia.sound": "multimedia-volume-control",
+    "multimedia.video": "camera-video",
+    "multimedia.audio": "library-music",
+    "multimedia.graphics": "camera-photo",
+    "network.download": "transmission",
+    "network.email": "internet-mail",
+    "network.im": "empathy",
+    "network.irc": "hexchat",
+    "network.news": "internet-news-reader",
+    "network.web": "emblem-web",
+    "network.web.browser": "web-browser",
+    "office": "x-office-spreadsheet",
+    "office.finance": "homebank",
+    "office.maths": "gnome-calculator",
+    "office.scientific": "applications-science",
+    "office.notes": "gnote",
+    "office.viewers": "calibre-viewer",
+    "programming.devel": "text-x-changelog",
+    "programming.haskell": "applications-haskell",
+    "programming.ide": "text-editor",
+    "programming.java": "applications-java",
+    "programming.python": "application-x-python-bytecode",
+    "programming.tools": "gitg",
+    "security": "preferences-system-firewall",
+}
+
+
 def is_essential_package(pkg):
     """ Essential packages should NEVER be removed by the user. """
     if pkg.partOf in essential_components:
@@ -161,6 +213,8 @@ class EopkgComponent(ProviderCategory):
         return str(self.id)
 
     def get_icon_name(self):
+        if str(self.id) in ICON_MAPS:
+            return ICON_MAPS[str(self.id)]
         return "package-x-generic"
 
 
