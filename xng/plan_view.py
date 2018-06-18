b00c13dec70907ffb69bc3f565fa347569776336
Ikey Doherty <ikey@solus-project.com>
2018-06-18 18:17:10 +0100

    plan_view: Placeholder labels
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- xng/plan_view.py
+++ xng/plan_view.py
@@ -25,10 +25,36 @@ class ScPlanView(Gtk.Box):
     item = None  # Currently active item for planning
     operation_type = None  # Currently requested operation type
 
+    label_install = None  # Label for packages being installed
+    label_removal = None  # Label for packages being removed
+    label_upgrade = None  # Label for packages being upgraded
+
+    listbox_install = None  # Installations
+    listbox_removal = None  # Removals
+    listbox_upgrade = None # Upgrades
+
     def __init__(self, context):
         Gtk.Box.__init__(self, orientation=Gtk.Orientation.VERTICAL)
         self.context = context
         self.set_border_width(10)
+
+        self.label_install = Gtk.Label.new(
+            _("The following software will be installed"))
+        self.pack_start(self.label_install, False, False, 0)
+        self.label_install.set_halign(Gtk.Align.START)
+        self.label_install.set_margin_top(6)
+
+        self.label_removal = Gtk.Label.new(
+            _("The following software will be removed"))
+        self.pack_start(self.label_removal, False, False, 0)
+        self.label_removal.set_halign(Gtk.Align.START)
+        self.label_removal.set_margin_top(6)
+
+        self.label_upgrade = Gtk.Label.new(
+            _("The following software will be upgraded"))
+        self.pack_start(self.label_upgrade, False, False, 0)
+        self.label_upgrade.set_halign(Gtk.Align.START)
+        self.label_upgrade.set_margin_top(6)
 
     def prepare(self, item, operation_type):
         """ Prepare to be shown on screen """
