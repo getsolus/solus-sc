2115ca469faa59e1426877bba7f03a13901fb18f
Pierre-Yves <pyu@riseup.net>
2020-01-08 17:59:58 +0100

    Update copyright year
    Signed-off-by: Pierre-Yves <pyu@riseup.net>

--- solus_sc/__init__.py
+++ solus_sc/__init__.py
@@ -3,7 +3,7 @@
 #
 #  This file is part of solus-sc
 #
-#  Copyright © 2013-2019 Solus
+#  Copyright © 2013-2020 Solus
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
