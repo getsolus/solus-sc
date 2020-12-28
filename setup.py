cc39697ddc4bb63bd32f97dc702714ffbe7d5cfc
Joshua Strobl <joshua@streambits.io>
2020-12-28 22:16:51 +0200

    Install eopkgassist conf to correct dbus-1 location

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     name            = "solus-sc",
     version         = "22.0",
     author          = "Solus",
-    author_email    = "root@getsol.us",
+    author_email    = "copyright@getsol.us",
     description     = ("Solus Software Center"),
-    license         = "GPL-2.0",
+    license         = "GPL-2.0-only",
     url             = "https://github.com/getsolus/solus-sc",
     packages        = ['solus_sc', 'eopkg_assist', 'solus_update'],
     scripts         = ['solus-sc', 'solus-update-checker'],
@@ -15,9 +15,9 @@ setup(
     package_data    = {'solus_sc': ['data/update_dialog.ui', 'data/styling.css', 'data/arc.css', 'data/settings.ui']},
     data_files      = [("/usr/share/applications", ["data/solus-sc.desktop"]),
                        ("/usr/share/dbus-1/system-services", ["data/dbus-1/system-services/com.solus_project.eopkgassist.service"]),
-                       ("/etc/dbus-1/system.d", ["data/system.d/com.solus_project.eopkgassist.conf"]),
-                       ("/usr/share/polkit-1/actions", ["data/polkit-1/actions/com.solus_project.eopkgassist.policy"]),
+                       ("/usr/share/dbus-1/system.d", ["data/system.d/com.solus_project.eopkgassist.conf"]),
                        ("/usr/share/glib-2.0/schemas", ["data/com.solus-project.software-center.gschema.xml"]),
+                       ("/usr/share/polkit-1/actions", ["data/polkit-1/actions/com.solus_project.eopkgassist.policy"]),
                        ("/usr/share/xdg/autostart", ["data/solus-update.desktop"]),
                        ("/usr/libexec", ["eopkg-assist-wrapper"])]
 )
