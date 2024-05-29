35f50c0b961fe8a204cc87b004813fd565bd9ac3
Thomas Staudinger <Staudi.Kaos@gmail.com>
2024-05-29 17:29:23 +0200

    Update license lists and license update script (#331)
    * sync_licenses.sh: Update to SPDX 3.x repository
    
    Signed-off-by: Thomas Staudinger <Staudi.Kaos@gmail.com>
    
    * Update license lists
    
    Signed-off-by: Thomas Staudinger <Staudi.Kaos@gmail.com>
    
    ---------
    
    Signed-off-by: Thomas Staudinger <Staudi.Kaos@gmail.com>

--- sync_licenses.sh
+++ sync_licenses.sh
@@ -5,19 +5,19 @@ fail_exit(){
     exit 1
 }
 
-git clone https://github.com/spdx/license-list.git --depth=1 || fail_exit "Failed to clone"
+git clone https://github.com/spdx/license-list-data.git --depth=1 || fail_exit "Failed to clone"
 
 if [[ -e "licenses.spdx" ]];
     then rm -v licenses.spdx
 fi
 
-pushd license-list
+pushd license-list-data/text
 
-echo "licenses = [" > ../licenses.spdx
+echo "licenses = [" > ../../licenses.spdx
 for i in *.txt ; do
     nom=`echo "$i" | sed 's@\.txt$@@'`
-    echo -e "    \"${nom}\"," >> ../licenses.spdx
+    echo -e "    \"${nom}\"," >> ../../licenses.spdx
 done
 popd
-rm -rf license-list
+rm -rf license-list-data
 echo "]" >> licenses.spdx
